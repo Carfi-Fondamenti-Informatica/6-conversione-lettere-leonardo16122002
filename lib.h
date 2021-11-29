2c019d5eb452b2f601c71c17dee3aadeea29283b
leonardo16122002 <78797793+leonardo16122002@users.noreply.github.com>
2021-11-29 16:10:08 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,1 +1,9 @@
+//
+// Created by leona on 29/11/2021.
+//
 
+#ifndef ESERCIZIO_VALUTATO_6_LIBRARY_H
+#define ESERCIZIO_VALUTATO_6_LIBRARY_H
+bool verifica(char);
+void table();
+#endif //ESERCIZIO_VALUTATO_6_LIBRARY_H

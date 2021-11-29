843a787ba131a953fdef3979826eeaac7a00a6d4
leonardo16122002 <78797793+leonardo16122002@users.noreply.github.com>
2021-11-29 16:10:23 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,17 @@
+//
+// Created by leona on 29/11/2021.
+//
 
+#include "library.h"
+#include <iostream>
+bool verifica(char a){
+    if((int(a)>=97&&int(a)<=122)||(int(a)>=65&&int(a)<90)){
+        return true;
+    }
+    return false;
+}
+void table(){
+    for(int i=0;i<255;i++){
+        std::cout<<i<<"  "<<char(i)<<std::endl;
+    }
+}

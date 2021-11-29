0ecba97f573923e451d404253219d0b8ea86d5a1
leonardo16122002 <78797793+leonardo16122002@users.noreply.github.com>
2021-11-29 16:09:24 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,22 @@
 #include <iostream>
+#include "library.h"
 
-int main(){
-  return 0;
+using namespace std;
+char a;
+int main() {
+
+    //table();
+    //do{
+        cin >> a;
+        if (verifica(a)) {
+            if (int(a) >= 97 && int(a) <= 122) {
+                a = char(int(a) - 32);
+            } else {
+                a = char(int(a) + 32);
+            }
+        }
+        cout << a<< endl;
+    //}while(a!=0);
+        return 0;
+
 }

079ac38851c8c0c11679a09ba517f0db46f9c6be
leonardo16122002 <78797793+leonardo16122002@users.noreply.github.com>
2021-11-29 16:12:21 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -2,8 +2,8 @@
 // Created by leona on 29/11/2021.
 //
 
-#ifndef ESERCIZIO_VALUTATO_6_LIBRARY_H
-#define ESERCIZIO_VALUTATO_6_LIBRARY_H
+#ifndef ESERCIZIO_VALUTATO_6_LIB_H
+#define ESERCIZIO_VALUTATO_6_LIB_H
 bool verifica(char);
 void table();
-#endif //ESERCIZIO_VALUTATO_6_LIBRARY_H
+#endif //ESERCIZIO_VALUTATO_6_LIB_H
